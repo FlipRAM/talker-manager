b1db7cd379687c820ae4b016dee96ae41546a4be
Felipe Rangel <feliperangel277@gmail.com>
2022-06-20 16:51:27 -0300

    Commit auth

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,16 +2,16 @@ const authMiddleware = (req, res, next) => {
   const emailRegex = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
   const { email, password } = req.body;
   if (!email) {
-    return res.status(400).json({ message: "O campo \"email\" é obrigatório" });
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
   if (!emailRegex.test(email)) {
-    return res.status(400).json({ message: "O \"email\" deve ter o formato \"email@email.com\"" });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (!password) {
-    return res.status(400).json({ message: "O campo \"password\" é obrigatório" });
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
   if (password.length < 6) {
-    return res.status(400).json({ message: "O \"password\" deve ter pelo menos 6 caracteres" });
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   
   next();
