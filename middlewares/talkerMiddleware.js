ccf4826317b4b3dccc1a2ce3dd72d96dcc66c40d
Felipe Rangel <feliperangel277@gmail.com>
2022-06-20 16:48:06 -0300

    Fix some string marks

--- middlewares/talkerMiddleware.js
+++ middlewares/talkerMiddleware.js
@@ -2,10 +2,10 @@ const nameMiddleware = (req, res, next) => {
   const { name } = req.body;
 
   if (!name) {
-    return res.status(400).json({ message: "O campo \"name\" é obrigatório" });
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
   }
   if (name.length < 3) {
-    return res.status(400).json({ message: "O \"name\" deve ter pelo menos 3 caracteres" });
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
 
   next();
@@ -15,7 +15,7 @@ const ageMiddleware = (req, res, next) => {
   const { age } = req.body;
 
   if (!age) {
-    return res.status(400).json({ message: "O campo \"age\" é obrigatório" });
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
   }
   if (age < 18) {
     return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
@@ -28,12 +28,12 @@ const talkMiddleware = (req, res, next) => {
   const { talk } = req.body;
   
   if (!talk) {
-    return res.status(400).json({ message: "O campo \"talk\" é obrigatório" });
+    return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
   }
   if (!talk.watchedAt) {
-    return res.status(400).json({ message: "O campo \"watchedAt\" é obrigatório" });
+    return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
   } if (!talk.rate && typeof talk.rate !== 'number') {
-    return res.status(400).json({ message: "O campo \"rate\" é obrigatório" });
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
   
   next();
@@ -45,10 +45,10 @@ const ifTalk = (req, res, next) => {
 
   if (!dateRegex.test(talk.watchedAt)) {
     return res.status(400).json({
-      message: "O campo \"watchedAt\" deve ter o formato \"dd/mm/aaaa\""
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
     });
   } if (parseInt(talk.rate, 10) < 1 || parseInt(talk.rate, 10) > 5) {
-    return res.status(400).json({ message: "O campo \"rate\" deve ser um inteiro de 1 à 5" });
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
 
   next();
